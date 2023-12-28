7c3936a8e2455bf7a623585b33592b1cb74331dd
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-28 19:57:22 +0400

    Update uslov.java

--- uslov.java
+++ uslov.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class uslov {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.print("Input number: ");
-int a = sc.nextInt();
-if(a > 2 && a < 9){
-System.out.println("Number more 2 and less 9");
-}
-else{
-System.out.println("Unknow number");
-}
-    
-}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Input number: ");
+    int a = sc.nextInt();
+    if (a > 2 && a < 9) {
+      System.out.println("Number more 2 and less 9");
+    } else {
+      System.out.println("Unknow number");
+    }
+
+  }
 }
